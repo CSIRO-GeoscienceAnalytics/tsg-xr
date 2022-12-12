3c26d0e6b28d5f3153bf45190ea4eb515a89c91e
Morgan Williams <morgan.williams@csiro.au>
2022-12-12 12:16:29 +0800

    Add find_TSG_datasets() to quickly identify TSG subdirectories

--- tsgxr/__init__.py
+++ tsgxr/__init__.py
@@ -1,5 +1,31 @@
+from pathlib import Path 
+
 from . import _version
 
 __version__ = _version.get_versions()["version"]
 
 from .read import load_tsg
+
+
+def find_TSG_datasets(parent_directory):
+    """
+    Check a directory for subdirectories containing Hylogger TSG datasets.
+
+    Parameters
+    ----------
+    parent_directory : str | pathlib.Path
+        Directory containing Hylogger datasets.
+
+    Returns
+    -------
+    dict
+        Dictionary mapping of hole names to subdirectories containing
+        Hylogger datasets (the directory to be passed to `pytsg` or
+        `tsg-xr`).
+    """
+    return {
+        fpath.stem.replace("_tsg", ""): fpath.parent
+        for fpath in sorted(
+            set([fpath for fpath in Path(parent_directory).glob("**/*_tsg.tsg")])
+        )
+    }
