1bad56a3ebc6d7ede11eec3a676400758deda92c
Morgan Williams <morgan.williams@csiro.au>
2023-05-01 12:19:04 +0800

    Add logging from pyrolite.util.log, --log option

--- tsgxr/cli.py
+++ tsgxr/cli.py
@@ -11,7 +11,12 @@ except ImportError:
         return iterable
 
 
+from .util import Handle
 from . import __version__, find_TSG_datasets, load_tsg
+
+
+logger = Handle(__name__)
+
 
 app = typer.Typer()
 
@@ -36,16 +41,26 @@ def TSG2zarr(
     subsample_image: int = typer.Option(
         10, help="Subsampling factor for imagery data."
     ),
+    log: str = typer.Option(
+        None,
+        help="Whether to use logging, and if so what level (DEBUG, INFO)",
+    ),
 ):
     """
     Convert TSG file(s) to Zarr.
     """
+    if log is not None:
+        Handle("tsgxr", level=log.upper())
     if (
         not tsgdir.is_dir() and tsgdir.suffix.lower() == ".tsg"
     ):  # pointing to a specific .tsg file
+        logger.info("Loading TSG file: {}".format(tsgdir.name))
         spectra = "TIR" if tsgdir.stem.lower().endswith("tir") else "NIR"
+        assert tsgdir.exists(), "Specified TSG file does not exist."
         datasets = {tsgdir.parent.stem: tsgdir.parent}
     else:
+        logger.info("Loading TSG files from directory: {}".format(tsgdir.name))
+        assert tsgdir.exists(), "Specified directory does not exist."
         datasets = find_TSG_datasets(tsgdir)
 
     if datasets:
@@ -58,10 +73,11 @@ def TSG2zarr(
                 index_coord=index_coord,
             )
             name = "_".join([str(ds.coords["hole"][0].values), spectra]) + ".zarr"
-            outdir = (
-                (output_dir / name) if output_dir is not None else d / name
-            )  # put it in the TSG directory if an output folder is not given
-            ds.to_zarr(outdir, mode="w")  # overwrite if needed
+            
+            # put it in the TSG directory if an output folder is not given
+            outdir =                 output_dir if output_dir is not None else d
+            logger.info("Creating Zarr archive {} in {}.".format(name, str(outdir)))
+            ds.to_zarr(outdir / name, mode="w")  # overwrite if needed
 
 
 def version_callback(value: bool):
